d5fb805897b578f6dc63bd57bb7c27d8b1cd7cbf
Jack Jefferson <jackjefferson96@hotmail.co.uk>
2017-10-13 13:11:06 +0100

    Added multithread support.
    The server will now accept multiple simultaneous connections. The server does not currently print client information, but this is (hopefully) temporary.

--- src/acceptconnections.c
+++ src/acceptconnections.c
@@ -1,14 +1,43 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
 #include "server.h"
 
+void *spawnclienthandler(void *data);
+
 void acceptconnections(int sockfd)
 {
-  int client;
-  struct sockaddr_in6 *addr = malloc(sizeof(struct sockaddr_in6));
-  socklen_t addrlen = sizeof(*addr);
+  /* loop until we get a SIGINT (or similar) */
+  while (1) {
+    /* somewhere to store client data */
+    struct clientinfo *client = malloc(sizeof(struct clientinfo));
 
-  /* loop forever, accepting a connection every time */
-  while ((client = accept(sockfd, (struct sockaddr*) addr, &addrlen)) >= 0)
-    handleclient(client, addr);
+    /* wait for, and accept, a connection */
+    if ((client->clientfd = accept(sockfd, (struct sockaddr*) &(client->addr),
+                                   &client->addrlen)) >= 0) {
+      /* thread for this client */
+      pthread_t clientthread;
+
+      /* do the work in a separate thread, so we can accept new clients here */
+      pthread_create(&clientthread, NULL, spawnclienthandler, (void *) client);
+    }
+    else {
+      perror("accept");
+      /* carry on anyway... */
+    }
+  }
+}
+
+void *spawnclienthandler(void *data)
+{
+  /* cast back to correct data type */
+  struct clientinfo *client = (struct clientinfo*) data;
+
+  /* spawn a thread */
+  handleclient(client->clientfd);
+
+  /* clean up and exit */
+  free(data);
+  pthread_exit(NULL);
 }
